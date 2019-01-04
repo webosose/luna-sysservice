38897dd2234997f00476d1812b99ad8d6d31f37b
yogish.s <yogish.s@lge.com>
2019-01-04 11:54:43 +0530

    Coverity Issue: Fix for useless-call & variable initialize
    :Release Notes:
    Error fetching function calls are logged & time zone varaible is
    initialized
    
    :Detailed Notes:
    The calls for error string verification was not logged.The calls are
    now logged & the uninitialized varaible of time zone is initialized
    
    :Testing performed:
    Build & verified successful
    
    :QA Notes:
    NA
    
    :Issues Addressed:
    [PLAT-70856] 132844 Useless call (luna-sysservice)
    [PLAT-70847] 133357 Uninitialized scalar variable (luna-sysservice)
    
    Change-Id: I870cd3f97ada2b0b9b0684d8a84197cf0c9cff79

--- Src/ImageServices.cpp
+++ Src/ImageServices.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2010-2018 LG Electronics, Inc.
+// Copyright (c) 2010-2019 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -499,7 +499,7 @@ bool ImageServices::lsImageInfo(LSHandle* lsHandle, LSMessage* message,void* use
 	LS::Error error;
 	if (!LSMessageReply(lsHandle, message, reply.stringify().c_str(), error))
 	{
-		error.what();
+		qWarning() << error.what();
 	}
 
 	return true;
--- Src/TimePrefsHandler.cpp
+++ Src/TimePrefsHandler.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2010-2018 LG Electronics, Inc.
+// Copyright (c) 2010-2019 LG Electronics, Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -207,6 +207,7 @@ namespace {
 		tz.dstSupported = 0;
 		tz.offsetToUTC = 0;
 		tz.preferred = false;
+                tz.howManyZonesForCountry = 0;
 		return tz;
 	}
 } // anonymous namespace
